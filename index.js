30a32066b3aefd469e6795e946712523da5b5e67
Elijah Mbugi <mukundielijah@gmail.com>
2022-07-12 18:50:22 +0300

    POST method

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-const form = document.querySelector('.form');
-
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let name = document.querySelector('#name').value;
-    let estate = document.querySelector('#estate').value;
-    let stage = document.querySelector('#stage').value;
-    console.log(name, estate, stage);
-})
-
 //get Location
 
 const showPosition = (position) => {
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-    
-}
-const showError = (error) => {
-    console.log(error);
-}
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
 
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError);
-}
+  const form = document.querySelector(".form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let name = document.querySelector("#name").value;
+    let estate = document.querySelector("#estate").value;
+    let stage = document.querySelector("#stage").value;
+
+   //POST FORM DATA
+
+  fetch("https://cotsm-transport-form.herokuapp.com/members", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      estate: estate,
+      nearest_stage: stage,
+      latitude: latitude,
+      longitude: longitude,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  });
+
+  
+};
+const showError = (error) => {
+  alert("Geolocation is not supported by this browser.");
+};
