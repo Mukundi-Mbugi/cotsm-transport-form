c29bd19a8f476260ebef01cb111a1070f21d74fb
Elijah Mbugi <mukundielijah@gmail.com>
2022-07-14 21:01:59 +0300

    Post data to server

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 //get Location
-
-const showPosition = (position) => {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-
-  const form = document.querySelector(".form");
+let coordinates = [];
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(function(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    coordinates.push({
+      latitude: latitude,
+      longitude: longitude
+    })
+    
+  })
+  
+  const form = document.querySelector("#form");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -12,28 +19,25 @@ const showPosition = (position) => {
     let estate = document.querySelector("#estate").value;
     let stage = document.querySelector("#stage").value;
 
-   //POST FORM DATA
+    //POST FORM DATA
+    console.log(coordinates[0].latitude);
 
-  fetch("https://cotsm-transport-form.herokuapp.com/members", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      estate: estate,
-      nearest_stage: stage,
-      latitude: latitude,
-      longitude: longitude,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
+    fetch("https://cotsm-transport-form.herokuapp.com/members", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        estate: estate,
+        nearest_stage: stage,
+        latitude: coordinates[0].latitude,
+        longitude: coordinates[0].longitude,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    form.reset();
   });
-
   
-};
-const showError = (error) => {
-  alert("Geolocation is not supported by this browser.");
-};
+
