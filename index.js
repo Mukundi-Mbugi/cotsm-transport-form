66dcd978e81a36249dfd74391a3385ed2e9c6367
Elijah Mbugi <mukundielijah@gmail.com>
2022-07-14 22:11:11 +0300

    form data

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 //get Location
 let coordinates = [];
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(function(position) {
+  navigator.geolocation.getCurrentPosition(function (position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     coordinates.push({
       latitude: latitude,
-      longitude: longitude
-    })
-    
-  })
-  
-  const form = document.querySelector("#form");
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let name = document.querySelector("#name").value;
-    let estate = document.querySelector("#estate").value;
-    let stage = document.querySelector("#stage").value;
-
-    //POST FORM DATA
-    console.log(coordinates[0].latitude);
-
-    fetch("https://cotsm-transport-form.herokuapp.com/members", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        estate: estate,
-        nearest_stage: stage,
-        latitude: coordinates[0].latitude,
-        longitude: coordinates[0].longitude,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-    form.reset();
+      longitude: longitude,
+    });
   });
-  
 
+const form = document.querySelector("#form");
+let userName = document.querySelector("#name").value;
+let estate = document.querySelector("#estate").value;
+let stage = document.querySelector("#stage").value;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //POST FORM DATA
+  console.log(coordinates[0].latitude);
+
+  fetch("https://cotsm-transport-form.herokuapp.com/members", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      estate: estate,
+      nearest_stage: stage,
+      latitude: coordinates[0].latitude,
+      longitude: coordinates[0].longitude,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  form.reset();
+});
